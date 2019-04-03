604a0b00cc8d15e60e7dd22ab67e5e00e4e983a1
SA-lipengfei <3042636711@qq.com>
2019-04-03 15:55:34 +0800

    优化

--- src/fourOperation.java
+++ src/fourOperation.java
@@ -1,6 +1,6 @@
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -13,10 +13,10 @@ import java.util.Scanner;
  **/
 public class fourOperation {
     public static void main(String[] args){
-        int[] number = new int[5];
-        int[] symbol = new int[4];
-        int quesNumber;
+        int digitNumber=0;
+        int quesNumber=0;
         int count,count2,count3,count4;
+        Boolean flag = true;
         Map symbolMap = new HashMap();
         symbolMap.put("1","+");
         symbolMap.put("2","-");
@@ -24,31 +24,55 @@ public class fourOperation {
         symbolMap.put("4","/");
         Map questionMap = new HashMap();
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        System.out.println("请输入想要设置的题目数：");
-        quesNumber=scanner.nextInt();
-        String qusetion="";
-        System.out.println("产生的四则运算题如下：");
-        for(count=0;count<quesNumber;count++){
-            qusetion="";
-            for (count2=0;count2<5;count2++){
-                number[count2]= (int) (1+Math.random()*100);
+        System.out.println("欢迎使用自动出题系统!");
+        while (flag){
+            String qusetion="";
+            while (flag){
+                System.out.println("请输入想要设置的题目数：");
+                quesNumber=scanner.nextInt();
+                if (quesNumber>=1){
+                    break;
+                }
+                System.out.println("请输入有效数字!");
             }
-            for (count3=0;count3<4;count3++){
-                symbol[count3]= (int) (1+Math.random()*4);
+            while (flag){
+                System.out.println("请输入要操作的数目：");
+                digitNumber=scanner.nextInt();
+                if (digitNumber>=2){
+                    break;
+                }
+                System.out.println("请输入有效数字!");
             }
-            for(count4=0;count4<4;count4++){
-                qusetion+=String.valueOf(number[count4])+symbolMap.get(String.valueOf(symbol[count4]));
-                if (count4==3){
-                    qusetion+=String.valueOf(number[count4+1]);
+            int[] number = new int[digitNumber];
+            int[] symbol = new int[digitNumber-1];
+            System.out.println("产生的四则运算题如下：");
+            for(count=0;count<quesNumber;count++){
+                qusetion="";
+                for (count2=0;count2<digitNumber;count2++){
+                    number[count2]= (int) (1+Math.random()*100);
+                }
+                for (count3=0;count3<digitNumber-1;count3++){
+                    symbol[count3]= (int) (1+Math.random()*4);
+                }
+                for(count4=0;count4<digitNumber-1;count4++){
+                    qusetion+=String.valueOf(number[count4])+symbolMap.get(String.valueOf(symbol[count4]));
+                    if (count4==digitNumber-2){
+                        qusetion+=String.valueOf(number[count4+1]);
+                    }
+                }
+                Object qusIsExist = questionMap.get(qusetion);
+                if (qusIsExist==null){
+                    questionMap.put(qusetion,"0");
+                    System.out.println(count+1+". "+qusetion);
+
                 }
             }
-            Object qusIsExist = questionMap.get(qusetion);
-            if (qusIsExist==null){
-                questionMap.put(qusetion,"0");
-                System.out.println(count+1+". "+qusetion);
-
+            System.out.println("是否继续出题？ 是 --> 请输入\"1\"    否 --> 请输入\"0\"");
+            int result = scanner.nextInt();
+            if (result==0){
+                flag=false;
             }
         }
+        System.out.println("感谢使用");
     }
 }
